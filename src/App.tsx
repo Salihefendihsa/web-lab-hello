6a47f24db835a7d2401aafe43ea94e265e0e8802
Muhammed Talha Kavak <muhammed@example.com>
2026-02-17 14:21:55 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Web Tasarımı ve Programlama</h1>
+        <h2>Lab-1</h2>
+        <p>Ad Soyad: Muhammed Talha Kavak</p>
+        <p>Öğrenci No: 235541001</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
